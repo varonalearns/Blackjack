7d34e0e3da6d7286e5bcac0a67d59a0c26c5d669
Eva <evalearns@protonmail.com>
2022-10-18 08:38:09 -0700

    Fix issue with renderGame()

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let player = {
+    name: "Eva",
+    chips: 200
+}
 let cards = []
 let sum =  0
 let hasBlackJack = false
@@ -6,6 +10,9 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random() * 13 ) + 1
@@ -38,9 +45,10 @@ function newCard() {
 
 function renderGame() {
     cardsEl.textContent = "Cards: ";
-    for (let i = 0; cards.length; i++) {
+    for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
+    
     sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -52,5 +60,5 @@ function renderGame() {
         isAlive = false
     }
 
-    messageEl.textContent = message;
+    messageEl.textContent = message
 }
